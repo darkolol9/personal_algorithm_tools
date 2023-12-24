9fe727ea45763dab6a3a586c55570c1cb277adfd
HUSS <whinedine8@gmail.com>
2023-12-24 08:56:18 +0200

    S

---
+++ 25.py
@@ -0,0 +1,32 @@
+from algorithms.times import time_execution
+from functools import lru_cache
+
+
+
+@lru_cache
+def fib(n):
+    if n < 3:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
+
+
+
+
+
+
+@time_execution
+def sol():
+    i = 5
+
+
+    while len(str(fib(i))) < 1000:
+        i += 1
+
+
+    print(i)
+
+
+
+
+sol()
