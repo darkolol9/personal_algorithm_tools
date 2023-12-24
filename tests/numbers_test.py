cef5f1e998d10e5df0a72e7444bfabd075b8d6b0
HUSS <whinedine8@gmail.com>
2023-12-24 09:51:14 +0200

    S

---
+++ tests/numbers_test.py
@@ -0,0 +1,14 @@
+from algorithms._numbers import *
+
+
+
+
+def test_gcd():
+    assert gcd(15, 10) == 5
+    assert gcd(10, 15) == 5
+    assert gcd(15, 15) == 15
+
+
+def test_get_proper_divisors():
+    assert get_proper_divisors(15) == [1, 3, 5, 15]
+
